ce8aa3b3adc32ff17cccb77c21863e92ee67c2e4
Khushxal <vkkhushal18@gmail.com>
2024-06-22 15:30:46 +0530

    changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,64 +27,56 @@ function Contact() {
     }
   }
   return (
-    <div id="contact" className="container">
-      <div className="card my-1">
-        <div className="cover-container h-100 w-100 d-flex p-3 flex-column">
-          <h2>Contact Me</h2>
-          <hr />
-          <div className="row mt-5">
-            <div class="col-12 col-md-6 mx-auto">
-              <div class="card" id="contact-card">
-                <div class="card-img text-center">
-                  <img
-                    id="contact-card-img"
-                    src={img}
-                    alt="This is contact image"
-                    width="100%"
-                    height="100%"
-                  />
-                </div>
-                <div class="card-body">
-                  <form onSubmit={handleSubmit}>
-                    <div class="form-group">
-                      <div class="mb-3">
-                        <label
-                          for="exampleFormControlInput1"
-                          class="form-label"
-                        >
-                          Email address
-                        </label>
-                        <input
-                          type="email"
-                          name="email"
-                          className="form-control"
-                          id="exampleFormControlInput1"
-                          onChange={handleChange}
-                          value={details.email}
-                        />
-                      </div>
-                      <div class="mb-3">
-                        <label
-                          for="exampleFormControlInput1"
-                          class="form-label"
-                        >
-                          Phone
-                        </label>
-                        <input
-                          type="text"
-                          name="phone"
-                          className="form-control"
-                          id="exampleFormControlInput1"
-                          onChange={handleChange}
-                          value={details.phone}
-                        />
-                      </div>
+    <div id="contact" className="container card my-1">
+      <div className="cover-container h-100 w-100 d-flex p-3 flex-column">
+        <h2>Contact Me</h2>
+        <hr />
+        <div className="row mt-5">
+          <div class="col-12 col-md-6 mx-auto">
+            <div class="card" id="contact-card">
+              <div class="card-img text-center">
+                <img
+                  id="contact-card-img"
+                  src={img}
+                  alt="This is contact image"
+                  width="100%"
+                  height="100%"
+                />
+              </div>
+              <div class="card-body">
+                <form onSubmit={handleSubmit}>
+                  <div class="form-group">
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">
+                        Email address
+                      </label>
+                      <input
+                        type="email"
+                        name="email"
+                        className="form-control"
+                        id="exampleFormControlInput1"
+                        onChange={handleChange}
+                        value={details.email}
+                      />
                     </div>
-                    <div className="text-center">
-                      <button className="btn">Submit</button>
+                    <div class="mb-3">
+                      <label for="exampleFormControlInput1" class="form-label">
+                        Phone
+                      </label>
+                      <input
+                        type="text"
+                        name="phone"
+                        className="form-control"
+                        id="exampleFormControlInput1"
+                        onChange={handleChange}
+                        value={details.phone}
+                      />
                     </div>
-                  </form>
-                </div>
+                  </div>
+                  <div className="text-center">
+                    <button className="btn">Submit</button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
