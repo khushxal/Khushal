1eb66c7261ad437b0b20c2d246a85b0e2952ce42
Khushxal <vkkhushal18@gmail.com>
2024-08-20 16:36:28 +0530

    Changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Khushal/", // Change this to your repository name
+  // base: "/Khushal/", // Change this to your repository name
 });
