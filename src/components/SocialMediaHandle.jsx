d4755a250e6c6a2612165b8fa1be618b34280e18
Khushxal <vkkhushal18@gmail.com>
2024-08-30 14:41:46 +0530

    Changes

--- src/components/SocialMediaHandle.jsx
+++ src/components/SocialMediaHandle.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "../css/handle.css";
 import LinkedInAnimation from "../images/LinkedInAnimation.gif";
 import GithubAnimation from "../images/GithubAnimation.gif";
+import { isMobile } from "react-device-detect";
 function SocialMediaHandle() {
   return (
-    <div className="handle text-center">
+    <div className={isMobile ? "mobile" : "handle text-center"}>
       <a href="https://www.linkedin.com/in/khushal-verma">
-        <img src={LinkedInAnimation} width={60} />
+        <img src={LinkedInAnimation} width={100} />
       </a>
       <a href="https://github.com/khushxal?tab=repositories">
         <img src={GithubAnimation} width={50} />
